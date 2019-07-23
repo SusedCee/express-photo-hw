f708fad89718361b09666e5f392a482bb5c7ae9e
Sused Cabrera <susedcabrera@Suseds-MBP.domain>
2019-07-22 21:04:31 -0600

    made folders and set upserver and db

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+require('.db/db')
+
+const imagesController = require('./controllers/images');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/images', imagesController);
+
+// landing page we don't need a controller
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(3000, () => {
+	console.log('listening to port 3000')
+});
